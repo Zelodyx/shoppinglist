db0db4699153c9fc039d8562cf193b5569a9abe0
Zelody <zelody@Equipo1.com>
2023-03-28 14:49:45 -0600

    Inicializar el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+return(
+  <div className="container text-center">
+    <div className="row">
+      <div className="col">
+       <h1>Shopping List</h1>
+       <br />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        C.
+      </div>
+      <div className="col-8">
+        Primer elemento de mi lista
+      </div>
+      <div className="col-3">
+        Botones
+      </div>
+    </div>
+    <br />
+    <div className="row">
+      <div className="col">
+        C.
+      </div>
+      <div className="col-8">
+        Segundo elemento de mi lista
+      </div>
+      <div className="col-3">
+        Botones
+      </div>
+    </div>
+    <br />
+  </div>
+)
+}
+
+export default App
