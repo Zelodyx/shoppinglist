d621d0171e6db9389b4d3bddb0401bdd68fffd5d
Zelody <zelody@Equipo1.com>
2023-03-30 14:03:03 -0600

    Modificacion de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,45 @@ return(
     <div className="row">
       <div className="col">
        <h1>Shopping List</h1>
-       <br />
+       <hr />
       </div>
     </div>
     <div className="row">
-      <div className="col">
-        C.
+      <div className="col-1">
+        <input type="checkbox"/>
       </div>
-      <div className="col-8">
-        Primer elemento de mi lista
+      <div className="col text-start">
+        1 kg
       </div>
-      <div className="col-3">
-        Botones
+      <div className="col-5 col-md-7 text-start">
+        Tortillas
+      </div>
+      <div className="col-4 col-md-3 btn-group btn-group-sm text-end" role="group">
+        <button className="btn btn-outline-danger">
+          <i className="bi bi-trash2-fill"></i>
+        </button>
+        <button className="btn btn-outline-primary">
+          <i className="bi bi-pencil-square"></i>
+        </button>      
       </div>
     </div>
-    <br />
     <div className="row">
-      <div className="col">
-        C.
+      <div className="col-1">
+        <input type="checkbox"/>
       </div>
-      <div className="col-8">
-        Segundo elemento de mi lista
+      <div className="col text-start">
+        1 lt
       </div>
-      <div className="col-3">
-        Botones
+      <div className="col-5 col-md-7 text-start">
+        Aceite
+      </div>
+      <div className="col-4 col-md-3 btn-group btn-group-sm text-end" role="group">
+        <button className="btn btn-outline-danger">
+          <i className="bi bi-trash2-fill"></i>
+        </button>
+        <button className="btn btn-outline-primary">
+          <i className="bi bi-pencil-square"></i>
+        </button>      
       </div>
     </div>
     <br />
